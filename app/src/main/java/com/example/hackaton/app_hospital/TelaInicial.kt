37a8d8a64c6b73b6f889ad1bea4b89ee96517506
Hackaton FCamara - MI <teste@teste.com>
2018-10-27 13:08:35 -0300

    First Commit

---
+++ app/src/main/java/com/example/hackaton/app_hospital/TelaInicial.kt
@@ -0,0 +1,22 @@
+package com.example.hackaton.app_hospital
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaInicial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+    }
+
+    fun navegacaoEnfermagem() {
+        var intent = Intent(this, TelaListagem::class.java)
+        intent.putExtra("tipo", "enfermagem")
+    }
+    fun navegacaoLimpeza() {
+        var intent = Intent(this, TelaListagem::class.java)
+        intent.putExtra("tipo", "enfermagem")
+    }
+}
---
+++ app/src/main/java/com/example/hackaton/app_hospital/TelaListagem.kt
@@ -0,0 +1,12 @@
+package com.example.hackaton.app_hospital
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaListagem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_listagem2)
+    }
+}
